96eb6ae4dee83f418fc92dc05e3f188570da460a
deboralorayne <deboralorrayne23@gmail.com>
2024-12-05 17:34:36 -0300

    texto

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("ola mundão");
+let nome = "Jimin"
+let xpDoHeroi = 9568
+let nivel
+
+if (xpDoHeroi < 1000){
+    nivel = "Ferro"
+}    else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivel = "Bronze"  
+}   else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000){
+    nivel  = "Prata"
+}   else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000){
+    nivel = "Ouro"
+}   else if (xpDoHeroi >= 7001 &&  xpDoHeroi <= 8000){
+    nivel = "Platina"
+}   else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000){
+    nivel = "Ascendente"
+}   else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000){
+    nivel = "Imortal"
+}   else {
+    nivel = "Radiante"
+}
+
+console.log("O herói de nome " + nome + " está no nível de " + nivel + "." )
